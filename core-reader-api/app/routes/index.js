3ad2e1d7542aaafaee7d7db908aba683c5c41111
Alper Odaman <alperodaman@gmail.com>
2022-01-22 18:36:49 +0300

    added air logs router to index

--- core-reader-api/app/routes/index.js
+++ core-reader-api/app/routes/index.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import classes from "./classes.js";
 import schools from "./schools.js";
 import sensors from "./sensors.js";
+import airLogs from "./air-logs.js";
 
 const router = Router();
 
 router.use("/classes", classes);
 router.use("/schools", schools);
 router.use("/sensors", sensors);
-
+router.use("/air-logs", airLogs);
 export default router;
